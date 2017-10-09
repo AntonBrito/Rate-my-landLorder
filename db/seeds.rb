6e7fcce0e66e70402069765b2d5eec1f831101af
AntonBrito <tonybrito85@gmail.com>
2017-10-09 15:43:53 -0400

    Edited my seeds file
    Delete get-all file...

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(email: 'AntonBrito@gmail.com', password: '123', admin: true)
+landlord.create(name: 'Ellen', address: '29 Phillips st', user_id: 1)
+comment.create(subject: 'She didnt care about her place', comment: 'too much blabla', landlord_id: 1, rate: 1)

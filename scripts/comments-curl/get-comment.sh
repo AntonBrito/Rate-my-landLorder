7381fb22349c7ad5cf54ad444f2225d6c8204236
AntonBrito <tonybrito85@gmail.com>
2017-10-11 14:12:01 -0400

    I have my sigh up, in working
    Fix the sign up-in, now working on fixing my handlebars issue.

--- scripts/comments-curl/get-comment.sh
+++
@@ -1,8 +0,0 @@
-API="${API_ORIGIN:-http://localhost:4741}"
-URL_PATH="/comments"
-curl "${API}${URL_PATH}" \
-  --include \
-  --request GET \
-  --header "Authorization: Token token=$TOKEN"
-
-echo
